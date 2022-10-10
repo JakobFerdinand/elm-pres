6e644ed930a364a48d3b8ddc7048e4a733cf923f
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2022-10-10 21:02:34 +0200

    initial home page

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,22 @@
 module Pages.Home_ exposing (page)
 
-import Html
+import Element exposing (..)
+import Element.Font as Font
 import View exposing (View)
 
 
 page : View msg
 page =
     { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    , body =
+        column
+            [ centerX
+            , centerY
+            , spacing 30
+            ]
+            [ el [ Font.size 36, centerX ] <| text "ELM"
+            , paragraph []
+                [ text "If it compiles it works!"
+                ]
+            ]
     }
