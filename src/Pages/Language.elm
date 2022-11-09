c35baa0d0f5a28ada551c6882ad96833aa1603c6
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2022-11-09 12:38:57 +0100

    add language chart

--- src/Pages/Language.elm
+++ src/Pages/Language.elm
@@ -1,6 +1,10 @@
 module Pages.Language exposing (Model, Msg, layout, page)
 
 import Browser.Navigation as Nav
+import Chart as C
+import Chart.Attributes as CA
+import Chart.Events as CE
+import Chart.Item as CI
 import Effect exposing (Effect)
 import Element exposing (..)
 import Element.Font as Font
@@ -10,6 +14,7 @@ import Page exposing (Page)
 import Route exposing (Route)
 import Route.Path as Path
 import Shared
+import Svg as S
 import View exposing (Slide(..), View)
 
 
@@ -95,8 +100,49 @@ view model =
             , body =
                 column
                     [ spacing 20 ]
-                    [ text "comparison to others like C#, JavaScript and Haskell" ]
+                    [ text "comparison to others like C#, JavaScript and Haskell"
+                    , el
+                        [ width fill
+                        , height fill
+                        ]
+                      <|
+                        viewChart
+                    ]
             }
     , previous = Just NavigatePrevious
     , next = Just NavigateNext
     }
+
+
+viewChart : Element msg
+viewChart =
+    let
+        data =
+            [ { usability = 20, staticTypeSystem = 20, language = "JavaScript#" }
+            , { usability = 30, staticTypeSystem = 40, language = "TypeScript#" }
+            , { usability = 80, staticTypeSystem = 75, language = "C#" }
+            , { usability = 50, staticTypeSystem = 98, language = "Haskell" }
+            , { usability = 98, staticTypeSystem = 98, language = "Elm" }
+            ]
+    in
+    C.chart
+        [ CA.height 300
+        , CA.width 300
+        , CA.padding { top = 0, bottom = 0, left = 30, right = 10 }
+        ]
+        [ C.xLabels [ CA.color "white", CA.withGrid, CA.format (\_ -> "") ]
+        , C.yLabels [ CA.color "white", CA.withGrid, CA.format (\_ -> "") ]
+        , C.series .staticTypeSystem
+            [ C.scatter .usability [ CA.opacity 0.2, CA.borderWidth 1 ]
+                |> C.variation (\i d -> [ CA.size 150 ])
+            ]
+            data
+        , C.eachDot <|
+            \p dot ->
+                [ C.label
+                    [ CA.moveDown 5, CA.color (CI.getColor dot) ]
+                    [ S.text (CI.getData dot).language ]
+                    (CI.getCenter p dot)
+                ]
+        ]
+        |> Element.html
