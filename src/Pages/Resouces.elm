1813ecfc30fe27cbe5be6c3f8bfc36e1f6d5a57b
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2022-11-12 17:18:45 +0100

    Resources

--- src/Pages/Resouces.elm
+++ src/Pages/Resouces.elm
@@ -3,8 +3,10 @@ module Pages.Resouces exposing (Model, Msg, layout, page)
 import Browser.Navigation as Nav
 import Effect exposing (Effect)
 import Element exposing (..)
+import Element.Region exposing (description)
 import KeyListener
 import Layout exposing (Layout)
+import Logo
 import Page exposing (Page)
 import Route exposing (Route)
 import Route.Path as Path
@@ -91,8 +93,104 @@ view model =
     , body =
         Slide
             { header = text "Resources"
-            , body = none
+            , body =
+                viewLinks
             }
     , previous = Just NavigatePrevious
     , next = Just NavigateNext
     }
+
+
+viewLinks : Element msg
+viewLinks =
+    let
+        viewResource :
+            { url : String
+            , images :
+                List
+                    { imageUrl : String
+                    , imageDescription : String
+                    }
+            , description : String
+            }
+            -> Element msg
+        viewResource { url, images, description } =
+            newTabLink []
+                { url = url
+                , label =
+                    column [ width fill, spacing 5 ]
+                        [ row [ width (fill |> maximum 250), spacing 5 ]
+                            (images |> List.map viewImage)
+                        , el [ centerX ] <| text description
+                        ]
+                }
+
+        viewImage : { imageUrl : String, imageDescription : String } -> Element msg
+        viewImage { imageUrl, imageDescription } =
+            image [ width (fill |> maximum 250) ]
+                { src = imageUrl
+                , description = imageDescription
+                }
+    in
+    column [ width fill, height fill, spacing 50 ]
+        [ row [ width fill, spaceEvenly ]
+            [ newTabLink []
+                { url = "https://elm-lang.org/"
+                , label =
+                    column [ width fill, spacing 5 ]
+                        [ Logo.elm []
+                        , el [ centerX ] <| text "Elm Lang"
+                        ]
+                }
+            , viewResource
+                { url = "https://elmcraft.org/"
+                , images =
+                    [ { imageUrl = "/elmcraft-logo.png"
+                      , imageDescription = "elmcraft logo"
+                      }
+                    ]
+                , description = "Things built with elm: elmcraft.org"
+                }
+            , viewResource
+                { url = "https://elm-radio.com/"
+                , images =
+                    [ { imageUrl = "/elm-radio-logo.svg"
+                      , imageDescription = "elm-radio logo"
+                      }
+                    ]
+                , description = "Podcast: elm-radio"
+                }
+            ]
+        , row [ width fill, spaceEvenly ]
+            [ viewResource
+                { url = "https://elmprogramming.com/"
+                , images =
+                    [ { imageUrl = "elmprogramming-logo.png"
+                      , imageDescription = "elmprogramming logo"
+                      }
+                    ]
+                , description = "Tutorial: elmprogramming.com"
+                }
+            , viewResource
+                { url = "https://elm-lang.org/community"
+                , images =
+                    [ { imageUrl = "slack-logo.svg"
+                      , imageDescription = "slack logo"
+                      }
+                    , { imageUrl = "discord-logo.svg"
+                      , imageDescription = "discord logo"
+                      }
+                    ]
+                , description = "Community"
+                }
+            , viewResource
+                { url = "https://exercism.org/tracks/elm"
+                , images =
+                    [ { imageUrl = "/exercism-logo.svg"
+                      , imageDescription = "exercism logo"
+                      }
+                    ]
+                , description = "Exercism"
+                }
+            ]
+        ]
